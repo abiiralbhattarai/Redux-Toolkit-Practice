2d7050c36018d41eaad2a60c5df6fa832a17ab0c
abiiralbhattarai <bhattarai.abi07@gmail.com>
2022-05-27 14:24:15 +0545

    Code Update

---
+++ redux-app/src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+// import Cart from './pages/Cart';
+import NavBar from './components/NavBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <NavBar/>
+      <Routes>
+      <Route path="/" element={<Home />}></Route>
+      {/* <Route path="/cart" element={<Cart />}></Route> */}
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
